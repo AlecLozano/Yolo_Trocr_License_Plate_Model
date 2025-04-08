c1b678859447003b78ec930d98024d786b495cc7
AlecL <lozanoalec46@gmail.com>
2025-04-08 00:55:54 -0400

    [Fix]: Images passed to trocr are made to grayscale for more accuracy

--- Camera/live_cam.py
+++ Camera/live_cam.py
@@ -7,7 +7,7 @@ from torchvision import transforms
 import torch.nn.functional as F
 import io
 
-import Camera.utils as utils
+import utils
 
 '''
 
@@ -33,28 +33,31 @@ if not camera.isOpened():
 while True:
     # Capture the frame
     ret, frame = camera.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame_tensor = torch.from_numpy(frame).to(device)
+    frame_PIL = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)) # Turn fram to PIL
+    frame_PIL = ImageOps.grayscale(frame_PIL).convert("RGB") # Turn PIL into grayscale RGB image (For Trocr inferencing)
   
     if not ret:
         print("Error: Failed to grab frame.")
         break
 
     # Detect LP's from frame
-    pixel_values = yolo_processor(frame_tensor, return_tensors='pt').pixel_values.to(device)
+    pixel_values = yolo_processor(frame_PIL, return_tensors='pt').pixel_values.to(device)
     boxes = yolo_processor.post_process_object_detection(yolo_model(pixel_values), threshold=.9)[0]["boxes"]
     
+    #If yolo model detects something
     if boxes.size()[0] > 0:
-        boxes = utils.format_boxes(boxes, frame)
-        images = utils.crop_objects(boxes, frame,device)
+        boxes = utils.format_boxes(boxes, frame_PIL)
+        images = utils.crop_objects(boxes, frame_PIL)
+
+        #Ensure there are cropped images
         if images != None:
-            pixel_values = trocr_processor(images, return_tensors='pt').pixel_values.to(device)
+            pixel_values = trocr_processor(frame_PIL, return_tensors='pt').pixel_values.to(device)
             outputs = trocr_model.generate(pixel_values)
             labels = trocr_processor.post_process_image_text_to_text(outputs)
 
         frame = utils.draw_rectangle(frame, boxes, labels)
 
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    #frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
     cv2.imshow("output", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to exit
--- Camera/utils.py
+++ Camera/utils.py
@@ -1,34 +1,41 @@
 import cv2
 import torch
+from PIL import ImageOps, Image
+import random
 
 '''
 Util file thats used mainly for live_cam.py
 '''
 
-def crop_objects(boxes, image, device):
+def crop_objects(boxes, image):
     #padding_to = 400
     images = []
     for box in boxes:
-        x_min, y_min, x_max, y_max = box
+
+        x_min, y_min, x_max, y_max = box.tolist()
+
         # Crop image
         try:
-            images.append(torch.tensor(image[x_min:x_max, y_min:y_max]).to(device))
+            img_cropped = image.crop([x_min, y_min, x_max, y_max])
+            angle = random.uniform(-30,30)
+            images.append(ImageOps.grayscale(img_cropped).convert("RGB"))
         except:
-            print("box", [x_min, y_min, x_max, y_max], end='\n\n\n')
-            print(image)
-            return None
+            print(x_min, y_min, x_max, y_max)
+            print(box)
+            image.show()
+            exit()
 
     return images
 
 def format_boxes(boxes, image):
     new_boxes = []
     for box in boxes:
-        normalized = all(coord <= 1 for coord in box)
-            
+        normalized = len(box[box < 1]) == 4
+        
         x_min, y_min, x_max, y_max = box
 
         if normalized:
-            height, width, _ = image.shape
+            width, height = image.size
             x_min = int(x_min * width)
             y_min = int(y_min * height)
             x_max = int(x_max * width)
@@ -41,11 +48,16 @@ def format_boxes(boxes, image):
             y_min, y_max = y_max, y_min
         
         new_boxes.append([x_min, y_min, x_max, y_max])
-    return new_boxes
+    return torch.tensor(new_boxes)
 
 def draw_rectangle(image,boxes,labels):
     for i,box in enumerate(boxes):
-        x_min, y_min, x_max, y_max = box
+        try:
+            label = labels[i]
+        except:
+            label = None
+
+        x_min, y_min, x_max, y_max = box.tolist()
         image = cv2.rectangle(image, (x_min,y_max), (x_max,y_min), color=(0,0,255), thickness=2)
-        image = cv2.putText(image, labels[i], (x_min,y_max+25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+        image = cv2.putText(image, label, (x_min,y_max+25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
     return image
